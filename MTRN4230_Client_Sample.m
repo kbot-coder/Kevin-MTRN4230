a4e90118e00eff424a678957fd6227c8cd350c4c
thepoweranger <kevin.kadino@gmail.com>
2015-04-14 20:10:13 +1000

    server connect between robot studio and matlab
    Create the connection bettween the robot studio and matlab

---
+++ MTRN4230_Client_Sample.m
@@ -0,0 +1,46 @@
+% Script to communicate with IRB120 robot system
+% Mark Whitty
+% 140324
+function MTRN4230_Client_Sample
+
+% The robot's IP address.
+% robot_IP_address = '192.168.2.1';
+robot_IP_address = '127.0.0.1';
+
+% The port that the robot will be listening on. This must be the same as in
+% your RAPID program.
+robot_port = 1025;
+
+% Open a TCP connection to the robot.
+socket = tcpip(robot_IP_address, robot_port);
+set(socket, 'ReadAsyncMode', 'continuous');
+fopen(socket);
+
+% Check if the connection is valid.
+if(~isequal(get(socket, 'Status'), 'open'))
+    warning(['Could not open TCP connection A=to ', robot_IP_address, ' on port ', robot_port]);
+    return;
+end
+
+% Send a sample string to the server on the robot.
+% for i=1:30
+A=150; B=150; C=150; D=90; E=90; F=90;
+ 
+
+
+    
+POS = [A,B,C,D,E,F];
+robjoint = int2str(POS);
+% posi = [A,B,C];
+% pos = num2str(posi); 
+pos = strcat('[',int2str(A),',',int2str(B),',',int2str(C),']')
+fwrite(socket, pos);
+
+% Read a line from the socket. Note the line feed appended to the message in the RADID sample code.
+data = fgetl(socket);
+
+% Print the data that we got.
+fprintf(char(data));
+
+% Close the socket.
+fclose(socket);
