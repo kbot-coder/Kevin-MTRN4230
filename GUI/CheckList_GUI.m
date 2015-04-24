2b2965d473244d0bbf27ef6bf7a80a11a030a1a3
__IQBAL__ <m.i.aziz@student.unswq.edu.au>
2015-04-24 18:35:29 +1000

    comments
    comments

--- GUI/CheckList_GUI.m
+++ GUI/CheckList_GUI.m
@@ -1,30 +1,14 @@
-function varargout = CheckList_GUI(varargin)
-% CHECKLIST_GUI MATLAB code for CheckList_GUI.fig
-%      CHECKLIST_GUI, by itself, creates a new CHECKLIST_GUI or raises the existing
-%      singleton*.
-%
-%      H = CHECKLIST_GUI returns the handle to a new CHECKLIST_GUI or the handle to
-%      the existing singleton*.
-%
-%      CHECKLIST_GUI('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in CHECKLIST_GUI.M with the given input arguments.
-%
-%      CHECKLIST_GUI('Property','Value',...) creates a new CHECKLIST_GUI or raises the
-%      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before CheckList_GUI_OpeningFcn gets called.  An
-%      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to CheckList_GUI_OpeningFcn via varargin.
-%
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-%      instance to run (singleton)".
-%
-% See also: GUIDE, GUIDATA, GUIHANDLES
-
-% Edit the above text to modify the response to help CheckList_GUI
-
-% Last Modified by GUIDE v2.5 29-Mar-2015 22:00:53
+% CHECKLIST GUI
+% List the prerequisites before running the robot.
+%   All checkbox need to be clicked first before the push button can be
+%   clicked, closing the checklist window while opening the GUI to control
+%   the robot.
+%   Is the m file that need to be run first,
 
 % Begin initialization code - DO NOT EDIT
+function varargout = CheckList_GUI(varargin)
+
+
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
@@ -46,93 +30,37 @@ end
 
 % --- Executes just before CheckList_GUI is made visible.
 function CheckList_GUI_OpeningFcn(hObject, eventdata, handles, varargin)
-% This function has no output args, see OutputFcn.
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to CheckList_GUI (see VARARGIN)
-
-% Choose default command line output for CheckList_GUI
 handles.output = hObject;
-
-% Update handles structure
 handles.c=0;
 guidata(hObject, handles);
 
-% UIWAIT makes CheckList_GUI wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
 
 
-% --- Outputs from this function are returned to the command line.
 function varargout = CheckList_GUI_OutputFcn(hObject, eventdata, handles) 
-% varargout  cell array for returning output args (see VARARGOUT);
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Get default command line output from handles structure
 varargout{1} = handles.output;
 
 
-% --- Executes on button press in checkbox1.
 function checkbox1_Callback(hObject, eventdata, handles)
-% hObject    handle to checkbox1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hint: get(hObject,'Value') returns toggle state of checkbox1
-
-
-% --- Executes on button press in checkbox2.
 function checkbox2_Callback(hObject, eventdata, handles)
-% hObject    handle to checkbox2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hint: get(hObject,'Value') returns toggle state of checkbox2
-
-
-% --- Executes on button press in checkbox3.
 function checkbox3_Callback(hObject, eventdata, handles)
-% hObject    handle to checkbox3 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hint: get(hObject,'Value') returns toggle state of checkbox3
-
-
-% --- Executes on button press in checkbox4.
 function checkbox4_Callback(hObject, eventdata, handles)
-% hObject    handle to checkbox4 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hint: get(hObject,'Value') returns toggle state of checkbox4
-
-
-% --- Executes on button press in checkbox5.
 function checkbox5_Callback(hObject, eventdata, handles)
-% hObject    handle to checkbox5 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hint: get(hObject,'Value') returns toggle state of checkbox5
-
-
-% --- Executes on button press in checkbox6.
 function checkbox6_Callback(hObject, eventdata, handles)
-% hObject    handle to checkbox6 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% Hint: get(hObject,'Value') returns toggle state of checkbox6
+%---------------------------
 
 
-% --- Executes on button press in pushbutton2.
 function pushbutton2_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-if ((get(handles.checkbox1,'value'))+(get(handles.checkbox2,'value'))+(get(handles.checkbox3,'value'))+(get(handles.checkbox4,'value'))+(get(handles.checkbox5,'value'))+(get(handles.checkbox6,'value')))==6,
+% check if all every checkbox was clicked
+if ((get(handles.checkbox1,'value'))+...    
+        (get(handles.checkbox2,'value'))+...
+        (get(handles.checkbox3,'value'))+...
+        (get(handles.checkbox4,'value'))+...
+        (get(handles.checkbox5,'value'))+...
+        (get(handles.checkbox6,'value')))==6,
+    
+    % Run the new GUI and close the current checklist window.
     run GUI_Design_2015_04_8
     close CheckList_GUI
 end
+
+
