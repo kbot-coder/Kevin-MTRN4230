69debcadd5fff86d8a97d6b4c3ed35ab4d4ba5c1
thepoweranger <ronipermanasaputra@Computer.local>
2015-05-05 17:45:30 +1000

    resolution

--- GUI_Assignment4/ConnectToCamera.m
+++ GUI_Assignment4/ConnectToCamera.m
@@ -2,7 +2,7 @@ function [video1, video2, status]=ConnectToCamera()
 
 try
     % For windows
-    video1  = videoinput('winvideo',1);         % Input video 1 from table camera
+    video1  = videoinput('winvideo',1,'RGB24_900x1600');         % Input video 1 from table camera
     video2  = videoinput('winvideo',2);         % Input video 2 from conveyor camera
     status  = 1;                                % make the connection status = 1 ---> Connected
 catch
