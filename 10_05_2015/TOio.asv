57cc5a050848f78473e58f281a77c8820d87e9f3
thepoweranger <kevin.kadino@gmail.com>
2015-05-12 03:55:43 +1000

    function to ease matlab dude
    easy to use

---
+++ 10_05_2015/TOio.asv
@@ -0,0 +1,28 @@
+function [c,pos_theta] = TOio(Tro_ON_int,Tro_RUN_int,Vac_ON_int,Vac_RUN_int,speed_int)
+    
+    %convert to string
+    Tro_ON_str = int2str(Tro_ON_int);
+    Tro_RUN_str = int2str(Tro_RUN_int);
+    Vac_ON_str = int2str(Vac_ON_int);
+    Vac_RUN_str = int2str(Vac_RUN_int);
+    speed_str = int2str(speed_int);
+    
+    %put in correct format and sent to  robotstudio
+    wave_1 = strcat('[',Tro_ON_str,',',Tro_RUN_str,',',Vac_ON_str,',',...
+       Vac_RUN_str,',',speed_str,',0]');
+    c1=sender(wave_1);
+    pause(0.001);
+    c2=sender('STRIO');
+    x=0;
+    if strcmp(c1,'GREEN')==1
+        x=x+1;
+    end
+    if strcmp(c2,'GREEN')==1
+        x=x+1;
+    end
+    if x == 2
+        c= 'GREEN';
+        pos_theta = strcat('[',Tro_ON_str,',',Tro_RUN_str,',',Vac_ON_str,',',...
+       Vac_RUN_str,',',speed_str,']');
+    end
+end
---
+++ 10_05_2015/TOrobot.m
@@ -0,0 +1,34 @@
+function [c,pos_theta] = TOrobot(pos_x_int,pos_y_int,pos_z_int,thetaX_int,thetaY_int,thetaZ_int)
+    %convert angle to quat
+    quat_int=convertangle(thetaX_int,thetaY_int,thetaZ_int);
+    
+    %convert to string
+    pos_x_str = int2str(pos_x_int);
+    pos_y_str = int2str(pos_y_int);
+    pos_z_str = int2str(pos_z_int);
+     
+    %put in correct format and sent to  robotstudio
+    wave_1 = strcat('[',pos_x_str,',',pos_y_str,',',pos_z_str,']');
+    wave_2 = strcat('[',num2str(quat_int(1)),',',num2str(quat_int(2)),',',num2str(quat_int(3)),',',...
+        num2str(quat_int(4)),',','0,0]');
+    c1=sender(wave_1);
+    pause(0.001);
+    c2=sender(wave_2);
+    pause(0.001);
+    c3=sender('START');
+    x=0;
+    if strcmp(c1,'GREEN')==1
+        x=x+1;
+    end
+    if strcmp(c2,'GREEN')==1
+        x=x+1;
+    end
+    if strcmp(c3,'GREEN')==1
+        x=x+1;
+    end
+    if x == 3
+        c= 'GREEN';
+        pos_theta = strcat('[',pos_x_str,',',pos_y_str,',',pos_z_str,',',num2str(quat_int(1)),',',num2str(quat_int(2)),',',num2str(quat_int(3)),',',...
+        num2str(quat_int(4)),']');
+    end
+end
