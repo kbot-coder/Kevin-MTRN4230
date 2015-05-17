b6385711fb95e36c02cb799000b87568d4c6dc03
thepoweranger <kevin.kadino@gmail.com>
2015-05-17 12:01:34 +0700

    update to box_Counter

--- chocolate stack counter/box_counter.m
+++ chocolate stack counter/box_counter.m
@@ -2,6 +2,7 @@ function c=box_counter(s_mode,type,pos_coor)
 persistent box;
 
 if strcmp(s_mode,'WRITE')==1
+    if strcmp(pos_coor,'PACK')
         box_num = str2num(type);
         box(box_num,1)= box(box_num,1)+1;
         
@@ -10,6 +11,16 @@ if strcmp(s_mode,'WRITE')==1
             box(box_num,2) = box(box_num,2)+1;
             box(box_num,1)=1;
         end
+    elseif strcmp(pos_coor,'UNPACK')
+        box_num = str2num(type);
+        box(box_num,1)= box(box_num,1)-1;
+        
+        if box(box_num,1) == 0
+            
+            box(box_num,2) = box(box_num,2)-1;
+            box(box_num,1)=4;
+        end
+    end
         
         
 elseif strcmp(s_mode,'READ')==1
@@ -36,11 +47,11 @@ elseif strcmp(s_mode,'READ')==1
             end
         end
         
-        xnew_coor = box(number_read,3) + pos_min*length*cos(box(number_read,6));
-        ynew_coor = box(number_read,4) + pos_min*length*sin(box(number_read,6));
+        xnew_coor = box(number_read,3) + pos_min*length*sin(box(number_read,6));
+        ynew_coor = box(number_read,4) + pos_min*length*cos(box(number_read,6));
         znew_coor = box(number_read,5) + box(number_read,2)*3;
         c = [xnew_coor,ynew_coor,znew_coor,box(number_read,6)+90]; 
-        
+        c
 elseif strcmp(s_mode,'RESET')==1
     box = [];
        
@@ -66,13 +77,19 @@ elseif strcmp(s_mode,'MAKE')==1
         
     end
     
-elseif strcmp(s_mode,'SET')==1
+elseif strcmp(s_mode,'SETBOX')==1
     number_set = str2num(type);
     pos_coor_int = str2num(pos_coor);
     box(number_set,3) = pos_coor_int(1,1);
     box(number_set,4) = pos_coor_int(1,2);
     box(number_set,5) = pos_coor_int(1,3);
     box(number_set,6) = pos_coor_int(1,4);
+
+elseif strcmp(s_mode,'SETCHOC')==1
+    number_set = str2num(type);
+    pos_coor_int = str2num(pos_coor);
+    box(number_set,1) = pos_coor_int(1,1);
+    box(number_set,2) = pos_coor_int(1,2);
     
 elseif strcmp(s_mode,'DISPLAY')==1
     
